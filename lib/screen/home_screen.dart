3f4c3628dd8c52ec818cbb66086f06a0cf11a0bd
rosisneupane <rosisneupane1@gmail.com>
2025-04-27 17:24:06 +0545

    feat:home page

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:new_ui/config.dart';
 import 'package:new_ui/model/schedule.dart';
 import 'package:new_ui/screen/createschedule_screen.dart';
+import 'package:new_ui/screen/job_interview_roleplay_screen.dart';
+import 'package:new_ui/screen/social_scenario_screen.dart';
 import 'package:new_ui/widgets/drawer_widget.dart';
 import 'package:new_ui/widgets/home_top_bar.dart';
 import 'package:new_ui/widgets/mood_selector.dart';
@@ -40,12 +42,10 @@ class _HomeScreenState extends State<HomeScreen> {
         throw Exception('JWT Token not found');
       }
 
-      String url = AppConfig.apiUrl;
-
       final response = await http.get(
-        Uri.parse('$url/routines'),
+        Uri.parse('${AppConfig.apiUrl}/routines'),
         headers: {
-          'Authorization': 'Bearer $token', // Pass JWT Token in Header
+          'Authorization': 'Bearer $token',
           'Content-Type': 'application/json',
         },
       );
@@ -62,6 +62,12 @@ class _HomeScreenState extends State<HomeScreen> {
       }
     } catch (e) {
       debugPrint("Error fetching schedule: $e");
+      if (mounted) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+              content: Text('Failed to load schedule. Please try again.')),
+        );
+      }
       setState(() {
         isLoading = false;
       });
@@ -83,156 +89,201 @@ class _HomeScreenState extends State<HomeScreen> {
               },
             ),
             Expanded(
-              child: SingleChildScrollView(
-                padding: const EdgeInsets.symmetric(horizontal: 16),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const SizedBox(height: 40),
-                    const Text(
-                      "CUHK AI Therapist is\n here with you",
-                      style: AppTextStyles.question,
-                      textAlign: TextAlign.center,
-                    ),
-                    const SizedBox(height: 28),
-                    const MoodSelectorCard(),
-                    const SizedBox(height: 28),
+              child: RefreshIndicator(
+                onRefresh: fetchScheduleData,
+                child: SingleChildScrollView(
+                  physics: const AlwaysScrollableScrollPhysics(),
+                  padding: const EdgeInsets.symmetric(horizontal: 16),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const SizedBox(height: 40),
+                      const Text(
+                        "CUHK AI Therapist is here with you",
+                        style: AppTextStyles.question,
+                        textAlign: TextAlign.center,
+                      ),
+                      const SizedBox(height: 28),
+                      const MoodSelectorCard(),
+                      const SizedBox(height: 28),
 
-                    /// Weekly Highlight Section
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: const [
-                        Text(
-                          'What makes your week?',
-                          style: TextStyle(
-                            color: Color(0xFF4E3321),
-                            fontSize: 20,
-                            fontWeight: FontWeight.w500,
-                          ),
-                        ),
-                        Text(
-                          'View More...',
-                          style: TextStyle(
-                            color: Color(0xFF736A66),
-                            fontSize: 12,
-                          ),
-                        ),
-                      ],
-                    ),
-                    const SizedBox(height: 12),
+// Grid of Categories
+                      GridView.count(
+                        crossAxisCount: 2,
+                        shrinkWrap: true,
+                        physics: NeverScrollableScrollPhysics(),
+                        mainAxisSpacing: 16,
+                        crossAxisSpacing: 16,
+                        children: [
+                          FeatureContainer(
+                              icon: Icons.school, label: 'Education'),
+                          GestureDetector(
+                              onTap: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) =>
+                                          const JobInterviewRoleplayScreen()),
+                                );
+                              },
+                              child: FeatureContainer(
+                                  icon: Icons.work, label: 'Work')),
+                          GestureDetector(
+                              onTap: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) =>
+                                          const SocialScenarioPage()),
+                                );
+                              },
+                              child: FeatureContainer(
+                                  icon: Icons.group, label: 'Social')),
+                          GestureDetector(
+                              onTap: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) =>
+                                          const CreateScheduleScreen()),
+                                );
+                              },
+                              child: FeatureContainer(
+                                  icon: Icons.spa, label: 'Self Care')),
+                          FeatureContainer(
+                              icon: Icons.sports_esports, label: 'Leisure'),
+                          FeatureContainer(
+                              icon: Icons.chat_bubble_outline,
+                              label: 'EaseTalk'),
+                        ],
+                      ),
 
-                    /// Weekly Activities
-                    ...List.generate(4, (index) {
-                      return Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 8),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text(
-                              '19 Apr: Completed SEEM3510 Asm ${index + 1}',
-                              style: const TextStyle(
-                                color: Color(0xFF736A66),
-                                fontSize: 14,
-                                fontWeight: FontWeight.w700,
-                              ),
-                            ),
-                            Container(
-                              width: 22,
-                              height: 22,
-                              decoration: BoxDecoration(
-                                color: const Color(0xFFA694F5),
-                                borderRadius: BorderRadius.circular(100),
-                              ),
-                              child: const Icon(
-                                Icons.face,
-                                size: 16,
-                                color: Colors.white,
-                              ),
-                            ),
-                          ],
-                        ),
-                      );
-                    }),
+                      const SizedBox(height: 28),
 
-                    const SizedBox(height: 20),
-
-                    /// Schedule Header
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        const Text(
-                          'Schedule for the week ...',
-                          style: TextStyle(
-                            color: Color(0xFF4E3321),
-                            fontSize: 20,
-                            fontWeight: FontWeight.w500,
-                          ),
-                        ),
-                        GestureDetector(
-                          onTap: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => CreateScheduleScreen(),
-                              ),
-                            );
-                          },
-                          child: Text(
-                            'Create More...',
+                      // Schedule Header
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          const Text(
+                            'Schedule for the week ...',
                             style: TextStyle(
-                              color: Color(0xFF736A66),
-                              fontSize: 12,
+                              color: Color(0xFF4E3321),
+                              fontSize: 20,
+                              fontWeight: FontWeight.w500,
                             ),
                           ),
-                        ),
-                      ],
-                    ),
-                    const SizedBox(height: 20),
+                          TextButton(
+                            onPressed: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) =>
+                                      const CreateScheduleScreen(),
+                                ),
+                              );
+                            },
+                            child: const Text(
+                              'Create More...',
+                              style: TextStyle(
+                                color: Color(0xFF736A66),
+                                fontSize: 12,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 20),
 
-                    /// Horizontal Schedule Cards
-                    SizedBox(
-                      height: 150,
-                      child: isLoading
-                          ? const Center(child: CircularProgressIndicator())
-                          : scheduleData.isEmpty
-                              ? const Center(
-                                  child: Text(
-                                    "No Schedules",
-                                    style: TextStyle(
-                                      color: Colors.grey,
-                                      fontSize: 16,
-                                      fontWeight: FontWeight.w500,
-                                    ),
-                                  ),
-                                )
-                              : ListView.builder(
-                                  scrollDirection: Axis.horizontal,
-                                  itemCount: scheduleData.length,
-                                  itemBuilder: (context, index) {
-                                    final item = scheduleData[index];
-                                    return Padding(
-                                      padding:
-                                          const EdgeInsets.only(right: 12.0),
-                                      child: SizedBox(
-                                        width: 300,
-                                        child: ScheduleCard(
-                                          id: item.id,
-                                          date: item.date,
-                                          time: item.time,
-                                          text: item.text,
-                                          status: item.status,
+                      // Horizontal Schedule Cards
+                      SizedBox(
+                        height: 150,
+                        child: isLoading
+                            ? const Center(child: CircularProgressIndicator())
+                            : scheduleData.isEmpty
+                                ? Column(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      const SizedBox(height: 10),
+                                      const Text(
+                                        "No Schedules",
+                                        style: TextStyle(
+                                          color: Colors.grey,
+                                          fontSize: 16,
+                                          fontWeight: FontWeight.w500,
                                         ),
                                       ),
-                                    );
-                                  },
-                                ),
-                    ),
-                  ],
+                                    ],
+                                  )
+                                : ListView.builder(
+                                    scrollDirection: Axis.horizontal,
+                                    itemCount: scheduleData.length,
+                                    itemBuilder: (context, index) {
+                                      final item = scheduleData[index];
+                                      return Padding(
+                                        padding:
+                                            const EdgeInsets.only(right: 12.0),
+                                        child: SizedBox(
+                                          width: 300,
+                                          child: ScheduleCard(
+                                            id: item.id,
+                                            date: item.date,
+                                            time: item.time,
+                                            text: item.text,
+                                            status: item.status,
+                                          ),
+                                        ),
+                                      );
+                                    },
+                                  ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class FeatureContainer extends StatelessWidget {
+  final IconData icon;
+  final String label;
+  const FeatureContainer({super.key, required this.icon, required this.label});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(16),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.2),
+            spreadRadius: 2,
+            blurRadius: 5,
+          ),
+        ],
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(icon,
+              size: 36,
+              color: AppColors.primary), // Change primary color if needed
+          const SizedBox(height: 8),
+          Text(
+            label,
+            style: const TextStyle(
+              fontSize: 14,
+              fontWeight: FontWeight.w500,
+              color: Color(0xFF4E3321),
+            ),
+            textAlign: TextAlign.center,
+          ),
+        ],
       ),
     );
   }

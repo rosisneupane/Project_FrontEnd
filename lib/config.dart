7733c74af37ad85a64404fc0425ce53b89079e1c
rosisneupane <rosisneupane1@gmail.com>
2025-05-13 18:58:02 +0545

    feat:new url

--- lib/config.dart
+++ lib/config.dart
@@ -1,6 +1,6 @@
 class AppConfig {
   //static const String apiUrl = "http://10.0.2.2:8000";
   //static const String apiUrl = "http://192.168.100.115:8000";
-  static const String apiUrl = "https://backend-d3ph.onrender.com";
+  static const String apiUrl = "https://backend-qqiq.onrender.com";
   static const String apiKey = "your_api_key_here";
 }

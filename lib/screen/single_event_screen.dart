693c94c0a5c23fe2bf6c630f6e5e3ccb5f497511
rosisneupane <rosisneupane1@gmail.com>
2025-05-14 15:00:11 +0545

    feat:clickable link in event

--- lib/screen/single_event_screen.dart
+++ lib/screen/single_event_screen.dart
@@ -1,11 +1,28 @@
 import 'package:flutter/material.dart';
-import 'package:new_ui/screen/event_screen.dart';
-
+import 'package:flutter_linkify/flutter_linkify.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:new_ui/screen/event_screen.dart'; // Assumes your Event model is imported from here
 
 class SingleEventPage extends StatelessWidget {
   final Event event;
 
   const SingleEventPage({Key? key, required this.event}) : super(key: key);
+
+Future<void> _onOpenLink(LinkableElement link) async {
+  final Uri url = Uri.parse(link.url);
+
+  try {
+    if (!await launchUrl(
+      url,
+      mode: LaunchMode.externalApplication,
+    )) {
+      debugPrint('Could not launch ${link.url}');
+    }
+  } catch (e) {
+    debugPrint('Error launching ${link.url}: $e');
+  }
+}
+
 
   @override
   Widget build(BuildContext context) {
@@ -13,39 +30,46 @@ class SingleEventPage extends StatelessWidget {
       appBar: AppBar(title: Text(event.title)),
       body: Padding(
         padding: const EdgeInsets.all(24.0),
-        child: Container(
-          padding: const EdgeInsets.all(20),
-          width: double.infinity,
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(16),
-            boxShadow: const [
-              BoxShadow(
-                color: Colors.black12,
-                blurRadius: 10,
-                offset: Offset(0, 5),
-              ),
-            ],
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                event.title,
-                style: const TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
+        child: SingleChildScrollView(
+          child: Container(
+            padding: const EdgeInsets.all(20),
+            width: double.infinity,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(16),
+              boxShadow: const [
+                BoxShadow(
+                  color: Colors.black12,
+                  blurRadius: 10,
+                  offset: Offset(0, 5),
                 ),
-              ),
-              const SizedBox(height: 16),
-              Text(
-                event.description,
-                style: const TextStyle(
-                  fontSize: 16,
-                  color: Colors.black87,
+              ],
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  event.title,
+                  style: const TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
-              ),
-            ],
+                const SizedBox(height: 16),
+                Linkify(
+                  onOpen: _onOpenLink,
+                  text: event.description,
+                  style: const TextStyle(
+                    fontSize: 16,
+                    color: Colors.black87,
+                  ),
+                  linkStyle: const TextStyle(
+                    color: Colors.blue,
+                    decoration: TextDecoration.underline,
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
